f28d96dcee3d48a568382884a5d7ab6633819470
firefoxmmx2 <firefoxmmx@gmail.com>
2015-06-30 17:45:33 +0800

    update ocr test code

--- src/main/java/org/firefoxmmx/ocr/ImageFilter.java
+++ src/main/java/org/firefoxmmx/ocr/ImageFilter.java
@@ -4,7 +4,9 @@ import com.sun.rowset.FilteredRowSetImpl;
 import net.sourceforge.tess4j.util.ImageHelper;
 import net.sourceforge.tess4j.util.ImageIOHelper;
 
+import javax.crypto.AEADBadTagException;
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ImageFilter {
@@ -53,22 +55,54 @@ public class ImageFilter {
     }
   }
 
+  /**
+   * 复制图片
+   * @param image
+   * @return
+   */
   public static BufferedImage cloneImage(BufferedImage image) {
     return ImageHelper.cloneImage(image);
   }
 
+  /**
+   * 截取图片
+   * @param image 原图片
+   * @param offsetX 位置x
+   * @param offsetY 位置y
+   * @param width 宽度
+   * @param height 高度
+   * @return 截取的图片
+   */
   public static BufferedImage subImage(BufferedImage image, int offsetX, int offsetY, int width, int height) {
     return ImageHelper.getSubImage(image, offsetX, offsetY, width, height);
   }
 
+  /**
+   * 图片统一灰度值转化
+   * @param image
+   * @return
+   */
   public static  BufferedImage convertImageToGrayScale(BufferedImage image){
     return ImageHelper.convertImageToGrayscale(image);
   }
 
+  /**
+   * 图片缩放
+   * @param image 图片
+   * @param width 新的宽度
+   * @param height 新的高度
+   * @return 缩放后的图片
+   */
   public static BufferedImage imageScale(BufferedImage image, int width,int height) {
-    return ImageHelper.getScaledInstance(image,width,height);
+    return ImageHelper.getScaledInstance(image, width, height);
   }
 
+  /**
+   * 图片对比度设置
+   * @param image 原始图片
+   * @param rate 对比率
+   * @return 调整后的图片(引用原始图片)
+   */
   public static BufferedImage imageContrast(BufferedImage image,float rate) {
     for (int x = image.getMinX(); x < image.getWidth(); x++) {
       for (int y = image.getMinY(); y < image.getHeight(); y++) {
@@ -76,9 +110,70 @@ public class ImageFilter {
         int dataRed = image.getColorModel().getRed(data);
         int dataBlue = image.getColorModel().getBlue(data);
         int dataGreen = image.getColorModel().getGreen(data);
-
-        image.setRGB((dataRed << 24)*rate | dataGreen << 16 | dataBlue);
+        int dataAlpha = image.getColorModel().getAlpha(data);
+        Color dataColor = new Color(dataRed*rate,dataGreen*rate,dataBlue*rate,dataAlpha);
+        image.setRGB(x,y,dataColor.getRGB());
       }
     }
+
+    return image;
+  }
+
+  /**
+   * 图片亮度调整
+   * @param image
+   * @param brightness
+   * @return
+   */
+  public static BufferedImage imageBrightness(BufferedImage image,int brightness) {
+    for (int x = image.getMinX(); x < image.getWidth(); x++) {
+      for (int y = image.getMinY(); y < image.getHeight(); y++) {
+        Object data = image.getRaster().getDataElements(x, y, null);
+        int dataRed = image.getColorModel().getRed(data);
+        int dataBlue = image.getColorModel().getBlue(data);
+        int dataGreen = image.getColorModel().getGreen(data);
+        int dataAlpha = image.getColorModel().getAlpha(data);
+        int newRed = dataRed + brightness > 255 ? 255:dataRed+brightness;
+        int newBlue = dataBlue + brightness > 255 ? 255: dataBlue+brightness;
+        int newGreen = dataGreen + brightness > 255 ? 255: dataGreen+brightness;
+        Color dataColor = new Color(newRed,newGreen,newBlue,dataAlpha);
+        image.setRGB(x,y,dataColor.getRGB());
+      }
+    }
+
+    return image;
+  }
+
+  /**
+   * 获取图片亮度
+   * @param image
+   * @return
+   */
+  public static long imageBrightness(BufferedImage image){
+    long totalBrightness = 0;
+    long totalRed = 0;
+    long totalGreen = 0;
+    long totalBlue=0;
+    for (int x = image.getMinX(); x < image.getWidth(); x++) {
+      for (int y = image.getMinY(); y < image.getHeight(); y++) {
+        Object data = image.getRaster().getDataElements(x, y, null);
+        int dataRed = image.getColorModel().getRed(data);
+        int dataBlue = image.getColorModel().getBlue(data);
+        int dataGreen = image.getColorModel().getGreen(data);
+        int dataAlpha = image.getColorModel().getAlpha(data);
+        totalRed += dataRed;
+        totalGreen += dataGreen;
+        totalBlue += dataBlue;
+//        totalBrightness += dataColor.getRGB();
+      }
+    }
+
+    float avgRed = totalRed / (image.getHeight()*image.getWidth());
+    float avgGreen = totalGreen / (image.getWidth() * image.getHeight());
+    float avgBlue  =totalBlue / (image.getWidth() * image.getHeight());
+
+    long avgBrightNess = totalBrightness / ( image.getWidth() * image.getHeight());
+
+    return avgBrightNess;
   }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,6 @@
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
-import net.sourceforge.tess4j.util.ImageHelper;
-import net.sourceforge.tess4j.util.ImageIOHelper;
+import org.firefoxmmx.ocr.ImageFilter;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -14,43 +13,7 @@ public class Test {
   private static int[] BLACK = new int[]{0, 0, 0};
   private static int[] GRAY = new int[]{55,55,55};
   private static int[] ID_GRAY = new int[]{30,30,30};
-  public static Object darkestPoint(BufferedImage image) {
-    Object darkestPoint = image.getRaster().getDataElements(image.getMinX(),image.getMinY(),null);
-    for (int x = image.getMinX(); x < image.getWidth(); x++) {
-      for (int y = image.getMinY(); y < image.getHeight(); y++) {
-        Object data = image.getRaster().getDataElements(x,y,null);
-        int dataBlue=image.getColorModel().getBlue(data);
-        int dataRed=image.getColorModel().getRed(data);
-        int dataGreen=image.getColorModel().getGreen(data);
 
-        if(dataBlue < image.getColorModel().getBlue(darkestPoint) &&
-            dataRed < image.getColorModel().getRed(darkestPoint) &&
-            dataGreen < image.getColorModel().getGreen(darkestPoint) &&
-            dataBlue != BLACK[2] &&
-            dataRed != BLACK[0] &&
-            dataGreen != BLACK[1])
-          darkestPoint = data;
-      }
-    }
-
-    return darkestPoint;
-  }
-
-  public static void convertToBinary(BufferedImage image,int[] rgb ){
-    for (int x = image.getMinX(); x < image.getWidth(); x++) {
-      for (int y = image.getMinY(); y < image.getHeight(); y++) {
-        Object data = image.getRaster().getDataElements(x,y,null);
-        int blue= image.getColorModel().getBlue(data);
-        int red = image.getColorModel().getRed(data);
-        int green = image.getColorModel().getGreen(data);
-
-        if(red > rgb[0] && green > rgb[1] && blue > rgb[2])
-          image.getRaster().setPixel(x,y,WHITE);
-        else
-          image.getRaster().setPixel(x,y,BLACK);
-      }
-    }
-  }
   public static void main(String[] args) throws TesseractException, IOException {
     File imageFile = new File("/home/hooxin/Downloads/IDCARD1/0.tif");
 
@@ -58,30 +21,25 @@ public class Test {
     Tesseract tesseract = new Tesseract();
     tesseract.setDatapath("/usr/share/tessdata/");
     tesseract.setLanguage("chi_sim");
-    BufferedImage bufferedImage = ImageHelper.cloneImage(ImageIO.read(imageFile));
-    bufferedImage = ImageHelper.convertImageToGrayscale(bufferedImage);
+    BufferedImage bufferedImage = ImageFilter.cloneImage(ImageIO.read(imageFile));
+    bufferedImage = ImageFilter.convertImageToGrayScale(bufferedImage);
     //缩放到真实身份证大小
-    bufferedImage = ImageHelper.getScaledInstance(bufferedImage, 673, 425);
+    bufferedImage = ImageFilter.imageScale(bufferedImage, 673, 425);
     ImageIO.write(bufferedImage, "jpg", new FileOutputStream(imageFile.getParent() + "/1.jpg"));
     int limit=25;
-    BufferedImage contentImage = ImageHelper.getSubImage(bufferedImage,bufferedImage.getMinX(),bufferedImage.getMinY(),414,154);
-    convertToBinary(contentImage,GRAY);
-    BufferedImage birthImage=ImageHelper.getSubImage(bufferedImage,bufferedImage.getMinX(),154,414,54);
-    convertToBinary(birthImage,GRAY);
-    BufferedImage addressImage = ImageHelper.getSubImage(bufferedImage,bufferedImage.getMinX(),208,414,144);
+    BufferedImage contentImage = ImageFilter.subImage(bufferedImage, bufferedImage.getMinX(), bufferedImage.getMinY(), 414, 154);
+    System.out.println("contentImage Brightness = "+ImageFilter.imageBrightness(contentImage));
+    ImageFilter.convertToBinary(contentImage, GRAY);
+    BufferedImage birthImage=ImageFilter.subImage(bufferedImage, bufferedImage.getMinX(), 154, 414, 54);
+    System.out.println("birthImage Brightness = "+ ImageFilter.imageBrightness(birthImage));
+    ImageFilter.convertToBinary(birthImage, GRAY);
+    BufferedImage addressImage = ImageFilter.subImage(bufferedImage, bufferedImage.getMinX(), 208, 414, 144);
+    System.out.println("addressImage Brightness = "+ ImageFilter.imageBrightness(addressImage));
+    ImageFilter.convertToBinary(addressImage, GRAY);
 
-    convertToBinary(addressImage,GRAY);
-
-    BufferedImage idImage = ImageHelper.getSubImage(bufferedImage, bufferedImage.getMinX(), 354, bufferedImage.getWidth(), bufferedImage.getHeight() - 354);
-//    darkestPoint=darkestPoint(idImage);
-//    referenceColor=new int[] {
-//        idImage.getColorModel().getRed(darkestPoint),
-//        idImage.getColorModel().getGreen(darkestPoint),
-//        idImage.getColorModel().getBlue(darkestPoint)
-//    };
-//    System.out.println("referenceColor["+ referenceColor[0]+","+referenceColor[1]+","+referenceColor[2]+"]");
-
-    convertToBinary(idImage,ID_GRAY);
+    BufferedImage idImage = ImageFilter.subImage(bufferedImage, bufferedImage.getMinX(), 354, bufferedImage.getWidth(), bufferedImage.getHeight() - 354);
+    System.out.println("idImage Brightness = "+ ImageFilter.imageBrightness(idImage));
+    ImageFilter.convertToBinary(idImage, ID_GRAY);
     ImageIO.write(contentImage, "jpg", new FileOutputStream(imageFile.getParent() + "/"  + "contentImage.jpg"));
     String result = tesseract.doOCR(contentImage);
     System.out.println(result);
